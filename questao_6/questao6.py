6058ac67c7b0e94f09b76790153cacff57310943
Rafael <faelnek@gmail.com>
2023-12-31 13:56:09 -0300

    questao6

--- questao_6/questao6.py
+++ questao_6/questao6.py
@@ -5,7 +5,13 @@ from selenium.webdriver.chrome.service import Service
 import json 
 
 def questao6(nome_autor):
-    navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    options= webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    
+    navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     try:
         
